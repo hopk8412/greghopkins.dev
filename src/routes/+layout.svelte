2cb9403307257898c1b908a0b9f9dfe375095ecf
hopk8412 <l1onh3art@protonmail.com>
2023-02-28 20:54:44 -0500

    typo fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	<title>greghopkins.dev</title>
 </svelte:head>
 <script>
-	import NavBar from '../lib/components/NavBar.svelte';
+	import NavBar from '../lib/components/Navbar.svelte';
 </script>
 
 <NavBar />
